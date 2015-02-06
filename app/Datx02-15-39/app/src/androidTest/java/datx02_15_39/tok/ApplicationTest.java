64889d6c3c8a30afcdfbfb189f0f0afc854d91bb
Tomas Hasselquist <tomas@hasselquist.se>
2015-02-06 09:08:05 +0100

    Attempt at first project push

---
+++ app/Datx02-15-39/app/src/androidTest/java/datx02_15_39/tok/ApplicationTest.java
@@ -0,0 +1,13 @@
+package datx02_15_39.tok;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
