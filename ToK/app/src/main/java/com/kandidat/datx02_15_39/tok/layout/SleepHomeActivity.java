75a6bca613a3ec21cf00311a88e7039c61cf39e6
Adam Jilsén <adam.jilsen@gmail.com>
2015-02-17 14:12:16 +0100

    Navigation to sleep
    Fixed link from start to sleep

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/SleepHomeActivity.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/SleepHomeActivity.java
@@ -1,31 +1,26 @@
 package com.kandidat.datx02_15_39.tok.layout;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.kandidat.datx02_15_39.tok.R;
 
-
-public class MainActivity extends ActionBarActivity {
-
-    Button kostButton;
+public class SleepHomeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
+        setContentView(R.layout.activity_sleep_home);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hello_world, menu);
+        getMenuInflater().inflate(R.menu.menu_sleep_home, menu);
         return true;
     }
 
@@ -42,26 +37,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void OnDietButtonClick(View view) {
-
-        //kostButton = (Button) findViewById(R.id.diet_button);
-
-        Intent dietIntent = new Intent(this, DietHomeActivity.class);
-
-        startActivity(dietIntent);
-
-
-
-    }
-
-    /**
-     * Navigates to sleep activity overview.
-     *
-     * @param view Not used.
-     */
-    public void OnSleepButtonClick(View view) {
-        startActivity(new Intent(this, SleepHomeActivity.class));
     }
 }
