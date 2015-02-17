3a50c78c13ef8a9ef82eabd497c315f7cba9d43d
Johan <nordholts.johan@gmail.com>
2015-02-17 14:05:52 +0100

    added MainActivity

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/MainActivity.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/MainActivity.java
@@ -1,14 +1,19 @@
 package com.kandidat.datx02_15_39.tok.layout;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.kandidat.datx02_15_39.tok.R;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button kostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OnDietButtonClick(View view) {
+
+        //kostButton = (Button) findViewById(R.id.diet_button);
+
+        Intent dietIntent = new Intent(this, DietHomeActivity.class);
+
+        startActivity(dietIntent);
+
+
+
     }
 }
