61b005390a6746dd3838dcb9bed8653db229724f
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2015-02-20 11:34:27 +0100

    added a addworkout class

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/AddWorkoutActivity.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/AddWorkoutActivity.java
@@ -1,28 +1,27 @@
 package com.kandidat.datx02_15_39.tok.layout;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.kandidat.datx02_15_39.tok.R;
 
-
-public class MainActivity extends ActionBarActivity {
+public class AddWorkoutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
+        setContentView(R.layout.activity_add_workout);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hello_world, menu);
+        getMenuInflater().inflate(R.menu.menu_add_workout, menu);
         return true;
     }
 
@@ -40,28 +39,7 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void OnDietButtonClick(View view) {
-
-        //kostButton = (Button) findViewById(R.id.diet_button);
-
-        Intent dietIntent = new Intent(this, DietHomeActivity.class);
-
-        startActivity(dietIntent);
-
-
-
-    }
-
-    /**
-     * Navigates to sleep activity overview.
-     *
-     * @param view Not used.
-     */
-    public void OnSleepButtonClick(View view) {
-        startActivity(new Intent(this, SleepHomeActivity.class));
-    }
-    public void OnWorkOutButtonClick(View view){
+    public void addButtonOnClick(View view){
         startActivity(new Intent(this, WorkoutHomeActivity.class));
     }
 }
