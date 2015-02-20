4c190731dbadce8b5a4824080af8967cf57bfaa6
Tomas Hasselquist <tomas@hasselquist.se>
2015-02-20 14:13:01 +0100

    Added selectivity and diary

---
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/model/sleep/SleepDiary.java
@@ -0,0 +1,56 @@
+package com.kandidat.datx02_15_39.tok.model.sleep;
+
+import com.kandidat.datx02_15_39.tok.model.AbstractDiary;
+import com.kandidat.datx02_15_39.tok.model.EditActivityParams;
+import com.kandidat.datx02_15_39.tok.model.IDiary;
+import com.kandidat.datx02_15_39.tok.model.IDiaryActivity;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by tomashasselquist on 20/02/15.
+ */
+public class SleepDiary extends AbstractDiary {
+
+    private static IDiary instance;
+    private List<IDiaryActivity> activityList;
+
+    @Override
+    public IDiary getInstance() {
+        if(instance == null){
+            instance = new SleepDiary();
+        }
+        return instance;
+    }
+
+    @Override
+    public void addActivity(Date d, IDiaryActivity activity) {
+        addActivityToTable(d, activity);
+    }
+
+    @Override
+    public IDiaryActivity getActivity(String id) {
+        return null;
+    }
+
+    @Override
+    public void removeActivity(String id) {
+
+    }
+
+    @Override
+    public List<IDiaryActivity> showDaysActivities(Date day) {
+        return null;
+    }
+
+    @Override
+    public List<IDiaryActivity> showWeekActivities(Date start, Date end) {
+        return null;
+    }
+
+    @Override
+    public void editActivity(String id, EditActivityParams eap) {
+
+    }
+}
