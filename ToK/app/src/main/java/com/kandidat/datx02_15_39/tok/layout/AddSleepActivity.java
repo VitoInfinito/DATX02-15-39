8a29d224600ed92a4da28355c907f968c6dd7a25
Adam Jilsén <adam.jilsen@gmail.com>
2015-03-03 17:16:03 +0100

    REVOLUTIONIZED THE ADDING OF THE SLEEP!
    ZOMG, SUCH MUCH GREATNESS IT IS SO GREAT THAT IT IS GREAT!

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/AddSleepActivity.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/AddSleepActivity.java
@@ -1,11 +1,13 @@
 package com.kandidat.datx02_15_39.tok.layout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 import com.kandidat.datx02_15_39.tok.R;
@@ -21,11 +23,31 @@ public class AddSleepActivity extends CustomActionBarActivity {
 
 	private Sleep newSleep;
 
+	private Date startDate;
+	private int startHours;
+	private int startMinutes;
+
+	private Date stopDate;
+	private int stopHours;
+	private int stopMinutes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sleep);
 		initMenu(R.layout.activity_add_sleep);
+
+	    startHours = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+	    startMinutes = Calendar.getInstance().get(Calendar.MINUTE);
+
+	    Button fromButton = (Button) findViewById(R.id.fromButton);
+	    fromButton.setText("From | " + startHours + ":" + startMinutes);
+
+	    stopHours = startHours+1;
+	    stopMinutes = startMinutes+20;
+
+	    Button toButton = (Button) findViewById(R.id.toButton);
+	    toButton.setText("To | " + stopHours + ":" + stopMinutes);
     }
 
 
@@ -52,7 +74,7 @@ public class AddSleepActivity extends CustomActionBarActivity {
     }
 
 	public void addNewSleep(){
-		Calendar cal = Calendar.getInstance();
+		/*Calendar cal = Calendar.getInstance();
 
 		TimePicker startTimePicker = (TimePicker) findViewById(R.id.startTimePicker);
 		TimePicker endTimePicker = (TimePicker) findViewById(R.id.endTimePicker);
@@ -64,7 +86,7 @@ public class AddSleepActivity extends CustomActionBarActivity {
 		int stopMinutes = endTimePicker.getCurrentMinute();
 
 		Date startDate = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), startHours, startMinutes).getTime();
-		Date stopDate = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), stopHours, stopMinutes).getTime();
+		Date stopDate = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), stopHours, stopMinutes).getTime();*/
 
 		Sleep sleep = new Sleep(startDate, stopDate);
 
@@ -75,8 +97,55 @@ public class AddSleepActivity extends CustomActionBarActivity {
 		sleepDiary.addActivity(startDate, sleepActivity);
 	}
 
+	public void fromButtonOnClick(View view){
+		AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+		builder.setTitle("Började sova");
+		builder.setIcon(R.drawable.zzz);
+
+		//NumberPicker weightPicker = new NumberPicker(this);
+		TimePicker startTimePicker = new TimePicker(this);
+
+		startTimePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+		startTimePicker.setCurrentMinute(Calendar.getInstance().get(Calendar.MINUTE));
+
+		builder.setView(startTimePicker);
+
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				Calendar cal = Calendar.getInstance();
+				startDate = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), startHours, startMinutes).getTime();
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// User cancelled the dialog
+			}
+		});
+		startTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener(){
+			@Override
+			public void onTimeChanged(TimePicker timepicker, int hours, int minutes){
+				startHours = hours;
+				startMinutes = minutes;
+
+				Button fromButton = (Button) findViewById(R.id.fromButton);
+				fromButton.setText("From | " + startHours + ":" + startMinutes);
+			}
+		});
+
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+
+
+	public void toButtonOnClick(View view){
+		//Date stopDate = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), stopHours, stopMinutes).getTime();
+
+	}
+
     public void addButtonOnClick(View view){
-		addNewSleep();
+
+		//addNewSleep();
         startActivity(new Intent(this, SleepHomeActivity.class));
     }
 }
