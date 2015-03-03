3464de9e787fcdc55013382b5a085bd03628415c
Tomas Hasselquist <tomas@hasselquist.se>
2015-03-03 11:31:15 +0100

    Added account and weight classes

---
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/model/weight/Weight.java
@@ -0,0 +1,7 @@
+package com.kandidat.datx02_15_39.tok.model.weight;
+
+/**
+ * Created by tomashasselquist on 03/03/15.
+ */
+public class Weight {
+}
---
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/model/weight/WeightDiary.java
@@ -0,0 +1,59 @@
+package com.kandidat.datx02_15_39.tok.model.weight;
+
+import com.kandidat.datx02_15_39.tok.model.AbstractDiary;
+import com.kandidat.datx02_15_39.tok.model.EditActivityParams;
+import com.kandidat.datx02_15_39.tok.model.IDiary;
+import com.kandidat.datx02_15_39.tok.model.IDiaryActivity;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by tomashasselquist on 03/03/15.
+ */
+public class WeightDiary extends AbstractDiary {
+
+    private static IDiary instance;
+
+    protected WeightDiary() {
+        super();
+    }
+
+    public static IDiary getInstance() {
+        if(instance == null){
+            instance = new WeightDiary();
+        }
+        return instance;
+    }
+
+    @Override
+    public void addActivity(Date d, IDiaryActivity activity) {
+
+    }
+
+    @Override
+    public IDiaryActivity getActivity(Calendar c, String id) {
+        return null;
+    }
+
+    @Override
+    public void removeActivity(Calendar c, String id) {
+
+    }
+
+    @Override
+    public List<IDiaryActivity> showDaysActivities(Calendar day) {
+        return null;
+    }
+
+    @Override
+    public List<IDiaryActivity> showWeekActivities(Calendar start, Calendar end) {
+        return null;
+    }
+
+    @Override
+    public void editActivity(Calendar c, String id, EditActivityParams eap) {
+
+    }
+}
