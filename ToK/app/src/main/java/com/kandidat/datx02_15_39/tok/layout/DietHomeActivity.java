222afa8a06584b8f23c8eefaa949c0f60ce22d66
Johan <nordholts.johan@gmail.com>
2015-03-05 23:07:09 +0100

    Added temp graph to DietHome and changed appearence on day/week buttons.

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/DietHomeActivity.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/layout/DietHomeActivity.java
@@ -1,13 +1,18 @@
 package com.kandidat.datx02_15_39.tok.layout;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
 import com.kandidat.datx02_15_39.tok.R;
 
 import java.text.SimpleDateFormat;
@@ -15,17 +20,20 @@ import java.util.Calendar;
 
 public class DietHomeActivity extends CustomActionBarActivity {
 
-    RadioButton dayRadioButton;
-    RadioButton weekRadioButton;
-    Button dateButton;
-
+    private Button dayRadioButton;
+    private Button weekRadioButton;
+    private Button dateButton;
+    private GraphView dietGraph;
+    private TextView kcalText, carbText, protText, fatText;
     private Calendar cal;
+    private BarGraphSeries<DataPoint> series;
+
+    private int foodCal = 500, foodProt = 30, foodFat = 50, foodCarb = 50;
 
     private int dayOffset = 0;
     private int weekOffset = 0;
 
     private SimpleDateFormat sdfShowFullDate = new SimpleDateFormat("yyyy-MM-dd");
-
 
 
     @Override
@@ -35,12 +43,41 @@ public class DietHomeActivity extends CustomActionBarActivity {
         setContentView(R.layout.activity_diet_home);
         initMenu(R.layout.activity_diet_home);
 
-        dayRadioButton = (RadioButton) findViewById(R.id.day_radioButton);
-        weekRadioButton = (RadioButton) findViewById(R.id.week_radiobutton);
+        dietGraph = (GraphView) findViewById(R.id.diet_graph);
+        series = new BarGraphSeries<>();
+
+        dayRadioButton = (Button) findViewById(R.id.day_radioButton);
+        weekRadioButton = (Button) findViewById(R.id.week_radiobutton);
         dateButton = (Button) findViewById(R.id.dateButton);
+        kcalText = (TextView) findViewById(R.id.kcal_text_view);
+        carbText = (TextView) findViewById(R.id.carb_text_view);
+        protText = (TextView) findViewById(R.id.protein_text_view);
+        fatText = (TextView) findViewById(R.id.fat_text_view);
 
         cal = Calendar.getInstance();
 
+        View.OnTouchListener dayAndWeekListener = new View.OnTouchListener() { //makes sure that not both day and week button are pressed simultaneously
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (v == dayRadioButton) {
+                    weekRadioButton.setPressed(false);
+                    v.setPressed(true);
+                    resetDay();
+
+                } else {
+                    dayRadioButton.setPressed(false);
+                    v.setPressed(true);
+                    resetWeek();
+                }
+                return true;
+            }
+        };
+        fillGraph(200, 30, 40, 50);
+
+        dayRadioButton.setPressed(true);
+        dayRadioButton.setOnTouchListener(dayAndWeekListener);
+        weekRadioButton.setOnTouchListener(dayAndWeekListener);
     }
 
 
@@ -62,22 +99,57 @@ public class DietHomeActivity extends CustomActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void initializeGraph() {
+
+
+        dietGraph = (GraphView) findViewById(R.id.diet_graph);
+
+        //TODO series.resetData på något snyggt vis.
+
+    }
+
+    private void fillGraph(int cal, int carb, int prot, int fat) {
+
+        series.resetData(new DataPoint[] {
+                new DataPoint(10,cal),
+                new DataPoint(20, carb),
+                new DataPoint(30, prot),
+                new DataPoint(40, fat)
+        });
+
+        series.setSpacing(10);
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+        dietGraph.addSeries(series);
+
+
+    } 
+
+//    private void printNutrients(int kcal, int carb, int prot, int fat) {
+//        kcalText.setText(kcal + " kcal");
+//        carbText.setText(carb + " g");
+//        protText.setText(prot + " g");
+//        fatText.setText(fat + "g ");
+//    }
+
     private boolean isDayView() {
-        return dayRadioButton.isChecked();
+        return dayRadioButton.isPressed();
     }
 
     public void onLeftButtonClick(View view) {
 
         if (isDayView()) {
-
             dayOffset--;
             cal.add(Calendar.DATE, -1); //Sets the date to one day from the current date
             String chosenDate = sdfShowFullDate.format(cal.getTime()); // sets to yyyy-mm-dd format
 
             if (dayOffset == -1) {
                 dateButton.setText("Igår");
+                fillGraph(500, 60, 30, 40);
             } else if (dayOffset == 0) {
                 dateButton.setText("Idag");
+                dateButton.setText("Igår");
+                fillGraph(500, 60, 30, 40);
             } else {
                 dateButton.setText(chosenDate);
             }
@@ -126,15 +198,6 @@ public class DietHomeActivity extends CustomActionBarActivity {
         }
     }
 
-    public void onDayViewClick(View view) {
-        resetDay();
-    }
-
-    public void onWeekViewClick(View view) {
-        resetWeek();
-    }
-
-
     public void onDateButtonClick(View view) {
         if (isDayView()) {
             resetDay();
@@ -154,5 +217,4 @@ public class DietHomeActivity extends CustomActionBarActivity {
         cal.setTime(Calendar.getInstance().getTime());
         dateButton.setText("Denna vecka");
     }
-
 }
