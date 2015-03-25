c928c40648be25416dfbf1d98a4a8b9249afbce2
Tomas Hasselquist <tomas@hasselquist.se>
2015-03-25 13:24:09 +0100

    Added utility class and two static variables to support Jawbone

---
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/utility/Utility.java
@@ -0,0 +1,18 @@
+package com.kandidat.datx02_15_39.tok.utility;
+
+/**
+ * Created by tomashasselquist on 25/03/15.
+ */
+public final class Utility {
+
+    public static final String CLIENT_ID = "9qZV-Q8xius";
+    public static final String CLIENT_SECRET = "413cad28a2662f161a8de887fe2d43d38b88f834";
+
+    private Utility() {}
+
+
+    public static final int getTwoPlusX(int x) {
+        return 3;
+    }
+
+}
