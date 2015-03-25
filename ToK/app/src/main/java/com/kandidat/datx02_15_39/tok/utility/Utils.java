d6619b9ddd6ffcea6f4eccee305f2e132d1c12ad
Tomas Hasselquist <tomas@hasselquist.se>
2015-03-25 15:03:58 +0100

    Added some classes for connecting jawbone to app

---
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/utility/Utils.java
@@ -0,0 +1,14 @@
+package com.kandidat.datx02_15_39.tok.utility;
+
+/**
+ * Created by tomashasselquist on 25/03/15.
+ */
+public final class Utils {
+
+    private Utils() {}
+
+
+    public static final int getTwoPlusX(int x) {
+        return 3;
+    }
+}
