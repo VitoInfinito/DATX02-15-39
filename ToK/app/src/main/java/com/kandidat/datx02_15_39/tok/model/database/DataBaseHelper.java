323d05cd9b4c55806a1f418060668c54923f1e2d
LagerstedtMarcus <marlag@student.chalmers.se>
2015-06-01 13:20:45 +0200

    Javadoc DataBaseHelper

--- ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/model/database/DataBaseHelper.java
+++ ToK/app/src/main/java/com/kandidat/datx02_15_39/tok/model/database/DataBaseHelper.java
@@ -14,9 +14,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is a helper class to be able to search in the databas aswell as create one if
+ * it is the first time launching the app.
+ */
 public class DataBaseHelper extends SQLiteOpenHelper
 {
-	private static String TAG = "DataBas"; // Tag just for the LogCat window
+	private static String TAG = "Database"; // Tag just for the LogCat window
 	private static String DB_NAME = "food_database.sql";// Database name
 	private static String DB_NAME_PART[] = {"food_database_vara.sql","food_database_nvarden.sql", "food_database_fpcu.sql"};
 	private SQLiteDatabase mDataBase;
@@ -28,8 +32,15 @@ public class DataBaseHelper extends SQLiteOpenHelper
 		mDataBase = mContext.openOrCreateDatabase(DB_NAME, SQLiteDatabase.CREATE_IF_NECESSARY, null);
 	}
 
+	/**
+	 * Method to search in the database with a keyword. The search function will search for words
+	 * that is close to the word text with sql statement %searchtxt% which search for items
+	 * that has the search text in them
+	 * @param searchtxt - The search text.
+	 * @return - list of items it found.
+	 */
 	public List<Food> searchFoodItems(String searchtxt){
-		Log.d("Databas", "Version" + getReadableDatabase().getVersion());
+		Log.d(TAG, "Version" + getReadableDatabase().getVersion());
 		ArrayList<Food> tmp = new ArrayList<>();
 		Cursor cur = selectWhereLikeStatement(searchtxt, "VABENAM", "Vara");
 		if(cur != null){
@@ -54,6 +65,14 @@ public class DataBaseHelper extends SQLiteOpenHelper
 		return tmp;
 	}
 
+	/**
+	 * Helper method to create food items from the search items that is in the cursor that is
+	 * gotten from the SQL SELECT FROM WHERE statement
+	 * @param cursor - cursor that can iterate through the objects that need to be transformed to a
+	 *               food object
+	 * @param name - the name of the food object
+	 * @return
+	 */
 	private Food getFood(Cursor cursor, String name){
 		if(cursor != null){
 			int calAm = 0, carbAm = 0, proAm = 0, fatAm= 0, amount = 100;
@@ -91,10 +110,23 @@ public class DataBaseHelper extends SQLiteOpenHelper
 		}
 	}
 
+	/**
+	 * Method to help doing a SELEC WHERE FROM Statement in the database.
+	 * @param whatToSelect - What search word you want to get from tables
+	 * @param columnName - which column name you want the what to select to be seleted from
+	 * @param fromTable - Which table to take information from
+	 * @return - cursor with the return table
+	 */
 	private Cursor selectWhereStatement(String whatToSelect, String columnName, String fromTable){
 		return getReadableDatabase().rawQuery("SELECT * FROM " + fromTable + " WHERE " + columnName + "='" + whatToSelect + "'", null);
 	}
-
+	/**
+	 * Method to help doing a SELEC WHERE FROM Statement in the database. With a Limit and LIKE isteed of exakt match.
+	 * @param whatToSelect - What search word you want to get from tables
+	 * @param columnName - which column name you want the what to select to be seleted from
+	 * @param fromTable - Which table to take information from
+	 * @return - cursor with the return table
+	 */
 	private Cursor selectWhereLikeStatement(String whatToSelect, String columnName, String fromTable){
 		return getReadableDatabase().rawQuery("SELECT * FROM " + fromTable + " WHERE " + columnName + " LIKE '%" + whatToSelect + "%' LIMIT 10", null);
 	}
@@ -109,7 +141,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		Log.d("Databas", "Skapar");
+		Log.d(TAG, "Skapar");
 		try {
 			insertFromFile(mContext, 1, db);
 		} catch (IOException e) {
@@ -117,11 +149,19 @@ public class DataBaseHelper extends SQLiteOpenHelper
 		}
 	}
 
+	/**
+	 * Method to help to reed from assest and copying from assest to the local database folder.
+	 * @param context
+	 * @param resourceId
+	 * @param db - database that you want to execute on
+	 * @return - the number of rows inserted.
+	 * @throws IOException - file not exist
+	 */
 	public int insertFromFile(Context context, int resourceId, SQLiteDatabase db) throws IOException {
 		// Reseting Counter
 		int result = 0;
 
-
+		//Reads from all sql files and takes out the sqlstatements from the lines and execute them
 		db.execSQL("BEGIN TRANSACTION;");
 		// Iterate through lines (assuming each insert has its own line and theres no other stuff)
 		for(int i = 0; i < 3; i++) {
@@ -138,7 +178,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
 							insertStmt += tmp != null? tmp: "";
 							insertStmtEnd = insertStmt.substring(insertStmt.length() - 2, insertStmt.length());
 						} else {
-							Log.d("Databas", "databas fel");
+							Log.d(TAG, "databas fel");
 						}
 					}
 					db.execSQL(insertStmt);
@@ -148,7 +188,6 @@ public class DataBaseHelper extends SQLiteOpenHelper
 			insertReader.close();
 		}
 		db.execSQL("COMMIT;");
-
 		// returning number of inserted rows
 		return result;
 	}
